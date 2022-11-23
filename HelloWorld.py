2e3bac913e903e8091741dfcb104e6504954d63d
heavychevy22 <a.elizondo@cox.net>
2022-11-23 16:47:36 -0700

    working on python_app

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+cars = ["chevelle", "corvette", "challenger", "roadrunner", "mustang"]
+print (cars)
+print (cars[0])
+print (cars[1])
+print (cars[2])
+print (cars[-1])
+print (cars[-2])
+print (cars[1:])
